34c0237131f44decfc94d6e8fcef65ca585d7b3c
txtcoder <tommytong.is.here+txtcoder@gmail.com>
2016-10-21 13:31:42 -0400

    x

--- src/main_page/client_info.controller.js
+++ src/main_page/client_info.controller.js
@@ -6,21 +6,35 @@
 
   ClientInfoController.$inject = ['CrmService', '$location','$scope','$timeout'];
   function ClientInfoController(CrmService, $location, $scope, $timeout) {
+    $scope.doSearch=false;
+
     $scope.contacts=[];
     $scope.editMode=false;
+    $scope.searchTerm= "";
     CrmService.fetchData().then(function(obj){
       $timeout(function(){
+        $scope.contacts=obj.data;
         $scope.curContact=obj.data[0];
       });
     });
 
-
+    $scope.toggleSearch = function(){
+      $scope.doSearch = !$scope.doSearch;
+    }
     $scope.edit = function(){
       $scope.editMode=true;
     }
     $scope.save = function(){
       $scope.editMode=false;
       CrmService.update($scope.curContact);
+    }
+
+    $scope.contactFilter = function(contact) {
+      console.log(contact);
+      return (contact.tcm_basic_contact.name.indexOf(searchTerm) !== -1);
+    }
+    $scope.updateCur = function(contact){
+      $scope.curContact=contact;
     }
   }
 })();
