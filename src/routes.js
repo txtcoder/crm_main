b51f0722109f499400be06ed7c6abfd60ac53bc2
txtcoder <tommytong.is.here+txtcoder@gmail.com>
2016-11-01 13:43:39 -0400

    add title

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,8 @@
   'use strict';
 
   angular.module('Crm')
-  .config(RoutesConfig);
+  .config(RoutesConfig)
+  .run(RouteChange);
 
   RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
 
@@ -12,24 +13,41 @@
     $stateProvider
     .state('login', {
       url: '/',
+      title: 'Login',
       templateUrl: 'src/login/login.html',
       controller: 'LoginController as login'
     })
     .state('client_info', {
       url: '/client_info',
+      title: 'Client Info',
       templateUrl: 'src/main_page/client_info.html',
       controller: 'ClientInfoController as client_info'
     })
     .state('contacts', {
       url: '/contacts',
+      title: 'Contact',
       templateUrl: 'src/contacts/contacts.html'
     })
 
     .state('agenda', {
       url: '/agenda',
+      title: 'Agenda',
       templateUrl: 'src/agenda/agenda.html'
     })
 
+
+
   }
+
+  RouteChange.$inject=['$rootScope'];
+  function RouteChange($rootScope) {
+      $rootScope.$on('$stateChangeSuccess', function (event, current, previous) {
+        console.log('hi');
+        console.log(current);
+        $rootScope.title = current.title;
+    })};
+
+
+
 
 })();
